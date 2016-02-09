d66cfd172ec1a8720e4477e0f20e899c6d1091ca
reshmavasudevan <reshma_vasudevan@yahoo.com>
2016-02-09 15:57:49 +0530

    done hopefully

---
+++ backend/application/controllers/Welcome.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function index()
+	{
+		$this->show();
+	}
+
+	public function show()
+	{
+		$this->load->model('dbuse');
+
+		$data['title']='Data from my database';
+		$data['array']=$this->dbuse->get();
+
+		$this->load->view('welcome_message',$data);
+	}
+}
